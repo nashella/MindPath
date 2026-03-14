cbd5952ba8b19eea4b4439fa94faa843773fc7c0
ashdot <ashlerose101@gmail.com>
2026-03-14 15:36:12 -0400

    added config for firebase

---
+++ firebaseconfig.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCqkXSKzeeQ6LeQvanp3QGuflKK5Era-4s",
+  authDomain: "mindpath-3dcb9.firebaseapp.com",
+  projectId: "mindpath-3dcb9",
+  storageBucket: "mindpath-3dcb9.firebasestorage.app",
+  messagingSenderId: "187899674876",
+  appId: "1:187899674876:web:5e493df001d7d408905779"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
